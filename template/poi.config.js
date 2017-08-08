d358a0297149ef50bcd315d4831cc76f01844e58
EGOIST <0x142857@gmail.com>
2017-08-08 17:52:33 +0800

    Update poi.config.js

--- template/poi.config.js
+++ template/poi.config.js
@@ -9,12 +9,12 @@ module.exports = {
   // In production mode
   // Electron can only load from relative path
   homepage: './',
-  // Exclude built-in Electron modules from bundle
+
   extendWebpack(config) {
     config.target('electron-renderer')
+    
     config.node
       .set('__filename', false)
       .set('__dirname', false)
-    return config
   }
 }
